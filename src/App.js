9b4c3984e0f07418b61b3a90d237eaac677529eb
Hacimüftüoglu <volkan.hacimueftueoglu@capgemini.com>
2018-12-11 16:43:59 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import Navbar from 'Navbar/Navbar';
+
+const localizer = BigCalendar.momentLocalizer(moment)
+
+const MyCalendar = props => (
+    <div>
+        <Navbar/>
+        <BigCalendar
+            localizer={localizer}
+            startAccessor="start"
+            endAccessor="end"
+        />
+    </div>
+)
 
 class App extends Component {
+
+
   render() {
-    return (
+      var myEventsList = []
+
+      return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+          <BigCalendar
+              localizer={localizer}
+              events={myEventsList}
+              startAccessor="start"
+              endAccessor="end"
+          />
       </div>
     );
   }
